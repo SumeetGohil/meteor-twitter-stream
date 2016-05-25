c9ca9405acf72c23b88d8fea92168dc7d7b43f61
Harshad Parulekar <harshadhparulekar@gmail.com>
2016-05-25 13:37:37 +0000

    Validating Twitter Keys.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+import chalk from 'chalk';
+import Twit from 'twit';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  chalk.enabled = true;
+
+  // Check if Twitter keys are present
+  if((!Meteor.settings.private.twitter.consumer_key || !Meteor.settings.private.twitter.consumer_secret) || 
+    (!Meteor.settings.private.twitter.consumer_key && !Meteor.settings.private.twitter.consumer_secret)) {
+      console.log(chalk.white.bgRed(" ERROR ") + chalk.red(" Twitter keys not found. Please check settings.json"));
+  }
+  else
+    console.log(chalk.white.bgGreen(" SUCCESS ") + chalk.green(" Twitter keys found."));
+
+  var T = new Twit(Meteor.settings.private.twitter);
+  T.get('search/tweets', { q: 'love', count: 1 }, function(err, data, response) {
+    if(err)
+      console.log(err)
+    if(err)
+      console.log(data)
+  });
 });
